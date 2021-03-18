39f9ce56adae08df0edd93e239805443095a0e14
Chris Tulin <47799223+christulin@users.noreply.github.com>
2021-03-18 15:03:16 -0700

    Finish seeding script for postgresql

--- database/seed.js
+++ database/seed.js
@@ -6,19 +6,20 @@ const sequelize = require('../database/Models');
 const seed = async () => {
 
   const photos = await listPhotos();
+  const objects = [];
 
   for (let i = 0; i < 1000; i++) {
     photos.Contents.forEach(photo => {
       const storage_url = `https://sdc-airbnb-photos.s3.us-east-2.amazonaws.com/${photo.Key}`
       const name = photo.Key.split('.')[0];
       const caption = faker.commerce.productName();
-      sequelize.models.photos.create({
-        storage_url,
-        name,
-        caption
-      });
+      const is_primary = i % 5 === 0;
+      objects.push({ storage_url, name, caption, is_primary });
     });
   };
+
+  sequelize.models.photos.bulkCreate(objects);
+
 };
 
 seed();
