4ad34947cbc35f06b16fb0431a1c69275427a9e3
Chris Tulin <47799223+christulin@users.noreply.github.com>
2021-04-08 21:11:23 -0700

    fix response to client, create connection uri for postgres

--- database/Models/index.js
+++ database/Models/index.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 const { applyAdditionalSetup } = require('./additional-setup');
 const dotenv = require('dotenv');
 
-const sequelize = new Sequelize({
+const sequelize = new Sequelize('postgres://postgres:postgres@localhost/sdc', {
   dialect: 'postgres',
   user: process.env.POSTGRES_USER,
   database: 'sdc',
